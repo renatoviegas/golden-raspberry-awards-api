edd9a73500fb1aa302175fd0289bf92e038cec5f
Renato Viegas <renato.viegas@hotmail.com>
2025-07-30 11:18:53 -0300

    test: adicionado/ajustado alguns testes

--- tests/integration/movie.integration.test.ts
+++ tests/integration/movie.integration.test.ts
@@ -10,25 +10,67 @@ describe('Movies API Integration', () => {
     );
   });
 
-  it('deve retornar todos os filmes do CSV', async () => {
+  it('deve retornar exatamente 206 filmes do CSV', async () => {
     const res = await request(app).get('/api/v1/movies');
     expect(res.status).toBe(200);
     expect(res.body.movies).toBeInstanceOf(Array);
+    expect(res.body.movies.length).toBe(206);
     expect(res.body.total).toBe(206);
   });
 
-  it('deve filtrar filmes vencedores', async () => {
-    const res = await request(app).get('/api/v1/movies?winner=true');
-    expect(res.status).toBe(200);
-    expect(res.body.movies.every((movie: any) => movie.winner)).toBeTruthy();
+  it('deve conter os campos obrigatórios em cada filme', async () => {
+    const res = await request(app).get('/api/v1/movies');
+    const movie = res.body.movies[0];
+    expect(movie).toHaveProperty('year');
+    expect(movie).toHaveProperty('title');
+    expect(movie).toHaveProperty('studios');
+    expect(movie).toHaveProperty('producers');
+    expect(movie).toHaveProperty('winner');
   });
 
-  it('deve retornar produtores com maior e menor intervalo', async () => {
+  it('deve conter como primeiro filme "Can\'t Stop the Music"', async () => {
+    const res = await request(app).get('/api/v1/movies');
+    const first = res.body.movies[0];
+    expect(first.year).toBe(1980);
+    expect(first.title).toBe("Can't Stop the Music");
+  });
+
+  it('deve retornar exatamente 43 filmes vencedores', async () => {
+    const res = await request(app).get('/api/v1/movies?winner=true');
+    expect(res.status).toBe(200);
+    expect(res.body.movies.length).toBe(43);
+    expect(res.body.movies.every((movie: any) => movie.winner === true)).toBe(
+      true
+    );
+  });
+
+  it('deve retornar produtores com menor e maior intervalo corretamente', async () => {
     const res = await request(app).get(
       '/api/v1/movies/producers-winners-intervals'
     );
     expect(res.status).toBe(200);
     expect(res.body).toHaveProperty('min');
     expect(res.body).toHaveProperty('max');
+
+    expect(Array.isArray(res.body.min)).toBe(true);
+    expect(Array.isArray(res.body.max)).toBe(true);
+    expect(res.body.min[0]).toHaveProperty('producer');
+    expect(res.body.min[0]).toHaveProperty('interval');
+    expect(res.body.min[0]).toHaveProperty('previousWin');
+    expect(res.body.min[0]).toHaveProperty('followingWin');
+
+    expect(res.body.min).toContainEqual({
+      producer: 'Bo Derek',
+      interval: 6,
+      previousWin: 1984,
+      followingWin: 1990,
+    });
+
+    expect(res.body.max).toContainEqual({
+      producer: 'Joel Silver',
+      interval: 10,
+      previousWin: 1990,
+      followingWin: 2000,
+    });
   });
 });
